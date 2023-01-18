4d2c07036caf10df5a756012de805fa3a53cb653
EricSlusarz <ericslusarz@gmail.com>
2023-01-18 11:29:43 +1100

    Adjusted Button Layout

--- js/main.js
+++ js/main.js
@@ -12,13 +12,13 @@ var wait = (ms) => {
 function addClass(elementToClass) {
     var x = document.getElementById("button1");
     var element = document.getElementById("firstInstructions");
-    element.classList.toggle("active") 
+    element.classList.toggle("active");
+    x.classList.toggle("activeBtn");
 
     if (x.innerHTML === 'Click Me To Close The Instructions <img src="https://www.htmlcssbuttongenerator.com/iconExample-plus-square-multiple-lined.svg" style="width:19px; margin-left:21px; margin-right:0px; flex-direction: row;">') {
         x.innerHTML = 'Click Me To Read The Instructions <img src="https://www.htmlcssbuttongenerator.com/iconExample-plus-square-multiple-lined.svg" style="width:19px; margin-left:21px; margin-right:0px; flex-direction: row;">'
     } else {
         x.innerHTML = 'Click Me To Close The Instructions <img src="https://www.htmlcssbuttongenerator.com/iconExample-plus-square-multiple-lined.svg" style="width:19px; margin-left:21px; margin-right:0px; flex-direction: row;">'
-
     }
 
 
