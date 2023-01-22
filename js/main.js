eee7d6623f557ab0d61dc79d35fba99e4ed9cb17
EricSlusarz <ericslusarz@gmail.com>
2023-01-22 19:45:22 +1100

    Refactored add class function/ added  XOR calculator fuctions

--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@ var wait = (ms) => {
 
 
 
-function addClass(elementToClass) {
-    var x = document.getElementById("button1");
-    var element = document.getElementById("firstInstructions");
+function addClass(buttonID, elementToClass) {
+    var x = document.getElementById(buttonID);
+    var element = document.getElementById(elementToClass);
     element.classList.toggle("active");
     x.classList.toggle("activeBtn");
 
@@ -20,6 +20,30 @@ function addClass(elementToClass) {
     } else {
         x.innerHTML = 'Click Me To Close The Instructions <img src="https://www.htmlcssbuttongenerator.com/iconExample-plus-square-multiple-lined.svg" style="width:19px; margin-left:21px; margin-right:0px; flex-direction: row;">'
     }
-
-
 }
+
+
+function toBinary (input) {
+    var result = "";
+    for (var i = 0; i < input.length; i++) {
+        var bin = input[i].charCodeAt().toString(2);
+        result += Array(8 - bin.length + 1).join("0") + bin;
+    } 
+    return result;
+}
+
+function convert() {
+    var output1 = document.getElementById("xorOutput1");
+    var output2 = document.getElementById("xorOutput2");
+    var output3 = document.getElementById("xorOutput3");
+
+    var input1 = document.getElementById("xorInput1").value;
+    var input2 = document.getElementById("xorInput2").value;
+    var input3 = document.getElementById("xorInput3");
+
+
+    input3.value = input1.charCodeAt(0) ^ input2.charCodeAt(0);
+    output3.value = toBinary(input3.value.toString());
+    output1.value = toBinary(input1);
+    output2.value = toBinary(input2);
+  }
